59e1b7d744f09d912dd2a7f904349c14017fb83a
Spencer King <spencer@spencertking.com>
2021-04-14 22:00:52 -0500

    Pull out diseases and their notes

--- js/data.js
+++ js/data.js
@@ -107,25 +107,45 @@ function getTopNDiseasesBySeverity(n, disSev) {
     return topn;
 }
 
+// Returns a key-value array of diseases and their notes
+function getDiseaseNotes(dataset) {
+    notes = {};
+    dataset.forEach(function(entry) {
+	name = entry['diseaseName'];
+	note = entry['Notes'];
+	if (notes[name] == null) {
+	    notes[name] = note;
+	} else {
+	    // TODO:
+	    // Can probably just remove this else and assume it'll all work fine
+	    if (notes[name] != note) {
+		console.log('This should never happen if I annotate the data right');
+	    }
+	}
+    });
+
+    return notes;
+}
+
 // Largely copied from: https://www.d3-graph-gallery.com/graph/pie_annotation.html
 function buildOrganPieChart(organs) {
-// set the dimensions and margins of the graph
-var width = 450
+    // set the dimensions and margins of the graph
+    var width = 450
     height = 450
     margin = 40
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+    // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+    var radius = Math.min(width, height) / 2 - margin
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#my_dataviz")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    // append the svg object to the div called 'my_dataviz'
+    var svg = d3.select("#my_dataviz")
+	.append("svg")
+	.attr("width", width)
+	.attr("height", height)
+	.append("g")
+	.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-// Prepare data
+    // Prepare data
     var data = {}; //organs;
 
     // Sort borrowed from:
@@ -146,59 +166,74 @@ var svg = d3.select("#my_dataviz")
     };
     data['Other'] = otherCount;
     
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(data)
-  .range(d3.schemeSet2);
+    // set the color scale
+    var color = d3.scaleOrdinal()
+	.domain(data)
+	.range(d3.schemeSet2);
 
-// Compute the position of each group on the pie:
-var pie = d3.pie()
-  .value(function(d) {return d.value; })
-var data_ready = pie(d3.entries(data))
-// Now I know that group A goes from 0 degrees to x degrees and so on.
+    // Compute the position of each group on the pie:
+    var pie = d3.pie()
+	.value(function(d) {return d.value; })
+    var data_ready = pie(d3.entries(data))
+    // Now I know that group A goes from 0 degrees to x degrees and so on.
 
-// shape helper to build arcs:
-var arcGenerator = d3.arc()
-  .innerRadius(0)
-  .outerRadius(radius)
+    // shape helper to build arcs:
+    var arcGenerator = d3.arc()
+	.innerRadius(0)
+	.outerRadius(radius)
 
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('mySlices')
-  .data(data_ready)
-  .enter()
-  .append('path')
-    .attr('d', arcGenerator)
-    .attr('fill', function(d){ return(color(d.data.key)) })
-    .attr("stroke", "black")
-    .style("stroke-width", "2px")
-    .style("opacity", 0.7)
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+	.selectAll('mySlices')
+	.data(data_ready)
+	.enter()
+	.append('path')
+	.attr('d', arcGenerator)
+	.attr('fill', function(d){ return(color(d.data.key)) })
+	.attr("stroke", "black")
+	.style("stroke-width", "2px")
+	.style("opacity", 0.7)
 
-// Now add the annotation. Use the centroid method to get the best coordinates
-svg
-  .selectAll('mySlices')
-  .data(data_ready)
-  .enter()
-  .append('text')
-  .text(function(d){ return d.data.key})
-  .attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
-  .style("text-anchor", "middle")
-  .style("font-size", 17)
+    // Now add the annotation. Use the centroid method to get the best coordinates
+    svg
+	.selectAll('mySlices')
+	.data(data_ready)
+	.enter()
+	.append('text')
+	.text(function(d){ return d.data.key})
+	.attr("transform", function(d) { return "translate(" + arcGenerator.centroid(d) + ")";  })
+	.style("text-anchor", "middle")
+	.style("font-size", 17)
 }
 
+
+
+
+// Diseases and their counts
 d = null;
+
+// Organs and their counts
 o = null;
+
+// Diseases and their severities
 s = null;
+
+// The top n diseases and their severities
 topNSeverities = null;
+
+// Diseases and their notes
+diseaseNotes = null;
 
 setTimeout(() => {
     d = uniqueDiseases(dataset);
     o = uniqueOrgans(dataset);
     s = diseaseSeverity(dataset);
     topNSeverities = getTopNDiseasesBySeverity(5, s);
+    diseaseNotes = getDiseaseNotes(dataset);
     buildOrganPieChart(o);
     console.log(d);
     console.log(o);
     console.log(s);
     console.log(topNSeverities);
+    console.log(diseaseNotes);
 }, 100);
