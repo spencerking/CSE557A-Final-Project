b472012635f92c6e09eddded324e8a1f51f12411
Spencer King <spencer@spencertking.com>
2021-04-19 20:28:12 -0500

    Display chromosome counts on overview

--- js/data.js
+++ js/data.js
@@ -70,6 +70,21 @@ function diseaseSeverity(dataset) {
     return severity;
 }
 
+// Returns a key-value array of chromosomes and their mutation counts
+function getChromosomeCounts(dataset) {
+    chromosomes = {};
+    dataset.forEach(function(entry) {
+	number = entry['chromosome'];
+	if (chromosomes[number] == null) {
+	    chromosomes[number] = 1;
+	} else {
+	    chromosomes[number] += 1;
+	}
+    });
+
+    return chromosomes;
+}
+
 function getTopNDiseasesBySeverity(n, disSev) {
     topn = {};
     ds = disSev;    
@@ -206,6 +221,67 @@ function buildOrganPieChart(organs) {
 	.style("font-size", 17)
 }
 
+/*
+// https://bl.ocks.org/d3noob/bdf28027e0ce70bd132edc64f1dd7ea4
+function buildChromosomeBarChart(chromosomes) {
+    // set the dimensions and margins of the graph
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scaleBand()
+        .range([0, width])
+        .padding(0.1);
+    var y = d3.scaleLinear()
+        .range([height, 0]);
+    
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.append("g")
+	.attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    // get the data
+    d3.csv("./data/chromosomes.csv").then(function(error, data) {
+	//if (error) throw error;
+
+	// format the data
+	data.forEach(function(d) {
+	    d.Count = +d.Count;
+	});
+
+	// Scale the range of the data in the domains
+	x.domain(data.map(function(d) { return d.Chromosome; }));
+	y.domain([0, d3.max(data, function(d) { return d.Count; })]);
+
+	// append the rectangles for the bar chart
+	svg.selectAll(".bar")
+	    .data(data)
+	    .enter().append("rect")
+	    .attr("class", "bar")
+	    .attr("x", function(d) { return x(d.Chromosome); })
+	    .attr("width", x.bandwidth())
+	    .attr("y", function(d) { return y(d.Count); })
+	    .attr("height", function(d) { return height - y(d.Count); });
+
+	// add the x Axis
+	svg.append("g")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(d3.axisBottom(x));
+
+	// add the y Axis
+	svg.append("g")
+	    .call(d3.axisLeft(y));
+
+    });
+}
+*/
+
 function displaySeverity(sevObj) {
     for (const [disease, severity] of Object.entries(sevObj)) {	
 	var fragment = document.createDocumentFragment();
@@ -216,13 +292,26 @@ function displaySeverity(sevObj) {
     }
 }
 
-
+function displayChromosomes(chrObj) {
+    i = 1;
+    for (const [chr, count] of Object.entries(chrObj)) {
+	var fragment = document.createDocumentFragment();
+	var element = document.createElement('span');
+	element.innerHTML = count;
+	fragment.appendChild(element);
+	document.getElementById("chr"+i.toString()).appendChild(fragment);
+	i++;
+    }
+}
 
 // Diseases and their counts
 d = null;
 
 // Organs and their counts
 o = null;
+
+// Chromosomes and their counts
+c = null;
 
 // Diseases and their severities
 s = null;
@@ -237,13 +326,17 @@ setTimeout(() => {
     d = uniqueDiseases(dataset);
     o = uniqueOrgans(dataset);
     s = diseaseSeverity(dataset);
+    c = getChromosomeCounts(dataset);
     topNSeverities = getTopNDiseasesBySeverity(5, s);
     diseaseNotes = getDiseaseNotes(dataset);
     buildOrganPieChart(o);
+    // buildChromosomeBarChart(c);
     console.log(d);
     console.log(o);
     console.log(s);
+    console.log(c);
     console.log(topNSeverities);
     console.log(diseaseNotes);
     displaySeverity(topNSeverities);
+    displayChromosomes(c);
 }, 100);
